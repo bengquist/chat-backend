5f847377d46f2b8432a192fe4f7b69cd402e7c02
bengquist <engquist_blake@yahoo.com>
2019-06-23 15:11:53 -0500

    set up authentication

--- src/index.js
+++ src/index.js
@@ -11,18 +11,72 @@ const app = express();
 app.use(cors());
 
 const server = new ApolloServer({
+  formatError: error => {
+    const message = error.message
+      .replace("SequelizeValidationError: ", "")
+      .replace("Validation error: ", "");
+
+    return {
+      ...error,
+      message,
+    };
+  },
   typeDefs: schema,
   resolvers,
-  context: {
+  context: async () => ({
     models,
-    me: { id: "1", username: "Blake" },
-  },
+    me: await models.User.findByLogin("rwieruch"),
+    secret: process.env.SECRET,
+  }),
 });
 
 server.applyMiddleware({ app, path: "/graphql" });
 
-sequelize.sync().then(async () => {
+const eraseDatabaseOnSync = true;
+
+sequelize.sync({ force: eraseDatabaseOnSync }).then(async () => {
+  if (eraseDatabaseOnSync) {
+    createUsersWithMessages();
+  }
+
   app.listen({ port: 8000 }, () => {
     console.log("Apollo Server on http://localhost:8000/graphql");
   });
 });
+
+const createUsersWithMessages = async () => {
+  await models.User.create(
+    {
+      username: "rwieruch",
+      email: "hello@robin.com",
+      password: "rwieruch",
+      messages: [
+        {
+          text: "Published the Road to learn React",
+        },
+      ],
+    },
+    {
+      include: [models.Message],
+    },
+  );
+
+  await models.User.create(
+    {
+      username: "ddavids",
+      email: "hello@david.com",
+      password: "ddavids",
+      messages: [
+        {
+          text: "Happy to release ...",
+        },
+        {
+          text: "Published a complete ...",
+        },
+      ],
+    },
+    {
+      include: [models.Message],
+    },
+  );
+};
--- src/resolvers/user.js
+++ src/resolvers/user.js
@@ -1,21 +1,75 @@
+import jwt from "jsonwebtoken";
+import { AuthenticationError, UserInputError } from "apollo-server";
+
+const createToken = async (user, secret, expiresIn) => {
+  const { id, email, username } = user;
+  return await jwt.sign({ id, email, username }, secret, {
+    expiresIn,
+  });
+};
+
 export default {
   Query: {
-    users: (parent, args, { models }) => {
-      return Object.values(models.users);
+    users: async (parent, args, { models }) => {
+      return await models.User.findAll();
     },
-    user: (parent, { id }, { models }) => {
-      return models.users[id];
+    user: async (parent, { id }, { models }) => {
+      return await models.User.findByPk(id);
     },
-    me: (parent, args, { me }) => {
-      return me;
+    me: async (parent, args, { models, me }) => {
+      if (!me) {
+        return null;
+      }
+
+      return await models.User.findByPk(me.id);
+    },
+  },
+
+  Mutation: {
+    signUp: async (
+      parent,
+      { username, email, password },
+      { models, secret },
+    ) => {
+      const user = await models.User.create({
+        username,
+        email,
+        password,
+      });
+
+      return { token: createToken(user, secret, "30m") };
+    },
+
+    signIn: async (
+      parent,
+      { login, password },
+      { models, secret },
+    ) => {
+      const user = await models.User.findByLogin(login);
+
+      if (!user) {
+        throw new UserInputError(
+          "No user found with these login credentials.",
+        );
+      }
+
+      const isValid = await user.validatePassword(password);
+
+      if (!isValid) {
+        throw new AuthenticationError("Invalid password.");
+      }
+
+      return { token: createToken(user, secret, "30m") };
     },
   },
 
   User: {
-    messages: (user, args, { models }) => {
-      return Object.values(models.messages).filter(
-        message => message.userId === user.id,
-      );
+    messages: async (user, args, { models }) => {
+      return await models.Message.findAll({
+        where: {
+          userId: user.id,
+        },
+      });
     },
   },
 };
